439fbe494f6e4bd06af58f8ad1ece6b80399bbc1
Jasper <jasper_hendrickx@hotmail.com>
2021-05-11 19:08:17 +0200

    Add user name in navbar and logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -6,6 +6,7 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
+  USER_LOGOUT,
 } from '../constants/userConstants';
 
 export const listUsers = () => async (dispatch) => {
@@ -62,4 +63,9 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
 };
