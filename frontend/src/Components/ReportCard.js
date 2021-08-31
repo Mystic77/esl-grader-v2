cb64c4e91b356087c9f9548567a9991bf147c692
Jasper <jasper_hendrickx@hotmail.com>
2021-08-31 16:07:16 +0200

    Add copy to clipboard functionality

--- frontend/src/Components/ReportCard.js
+++ frontend/src/Components/ReportCard.js
@@ -5,47 +5,63 @@ const ReportCard = ({ feedback, scores, commonErrors }) => {
   const scoresEntries = Object.entries(scores);
   const errorsEntries = Object.entries(commonErrors);
 
+  function copyToClip() {
+    let str = document.getElementById('feedback').innerHTML;
+
+    function listener(e) {
+      e.clipboardData.setData('text/html', str);
+      e.clipboardData.setData('text/plain', str);
+      e.preventDefault();
+    }
+    document.addEventListener('copy', listener);
+    document.execCommand('copy');
+    document.removeEventListener('copy', listener);
+  }
+
   return (
-    <div>
+    <>
       <Row className="my-3">
         <Col>
           <Button>Check Results</Button>
         </Col>
         <Col>
-          <Button>Copy To Clipboard</Button>
+          <Button onClick={copyToClip}>Copy To Clipboard</Button>
         </Col>
       </Row>
-      {feedback.map((feedbackItem) => (
-        <Card key={feedbackItem._id}>
-          {scoresEntries.map(
-            (result) =>
-              feedbackItem.category === result[0] && (
-                <>
-                  <Card.Header as="h2">
-                    {result[0]} {result[1]}/5
-                  </Card.Header>
-                  <Card.Body>
-                    <Card.Text>{feedbackItem.mainText[result[1]]}</Card.Text>
-                  </Card.Body>
-                </>
-              )
-          )}
-          {feedbackItem.commonErrors.map((commonError) =>
-            errorsEntries.map(
-              (error) =>
-                commonError.name === error[0] &&
-                error[1] === true && (
-                  <Card.Body>
-                    <Card.Text>
-                      {commonError.name}: {commonError.text}
-                    </Card.Text>
-                  </Card.Body>
+
+      <div id="feedback">
+        {feedback.map((feedbackItem) => (
+          <Card key={feedbackItem._id}>
+            {scoresEntries.map(
+              (result) =>
+                feedbackItem.category === result[0] && (
+                  <>
+                    <Card.Header as="h2">
+                      {result[0]} {result[1]}/5
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Text>{feedbackItem.mainText[result[1]]}</Card.Text>
+                    </Card.Body>
+                  </>
                 )
-            )
-          )}
-        </Card>
-      ))}
-    </div>
+            )}
+            {feedbackItem.commonErrors.map((commonError) =>
+              errorsEntries.map(
+                (error) =>
+                  commonError.name === error[0] &&
+                  error[1] === true && (
+                    <Card.Body>
+                      <Card.Text>
+                        {commonError.name}: {commonError.text}
+                      </Card.Text>
+                    </Card.Body>
+                  )
+              )
+            )}
+          </Card>
+        ))}
+      </div>
+    </>
   );
 };
 
