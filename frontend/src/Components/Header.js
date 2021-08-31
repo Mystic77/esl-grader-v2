bc5fc3e2e0c7eb34a0ee44c216b3b9bdeb6f8618
Jasper <jasper_hendrickx@hotmail.com>
2021-08-31 14:29:10 +0200

    Hide navbar elements if user is logged out

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -24,25 +24,28 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <LinkContainer to="/">
-                <Nav.Link>
-                  <i className="fas fa-home"></i> Grader
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/edit">
-                <Nav.Link>
-                  <i className="fas fa-edit"></i> Edit Feedback
-                </Nav.Link>
-              </LinkContainer>
               {userInfo ? (
-                <NavDropdown title={userInfo.name} id="username">
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                <>
+                  <LinkContainer to="/">
+                    <Nav.Link>
+                      <i className="fas fa-home"></i> Grader
+                    </Nav.Link>
                   </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
+                  <LinkContainer to="/edit">
+                    <Nav.Link>
+                      <i className="fas fa-edit"></i> Edit Feedback
+                    </Nav.Link>
+                  </LinkContainer>
+
+                  <NavDropdown title={userInfo.name} id="username">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
               ) : (
                 <LinkContainer to="/login">
                   <Nav.Link>
