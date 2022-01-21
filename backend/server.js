52f5440d5624e6638d8f521a4d9cef3c95f8edc7
Jasper <jasper_hendrickx@hotmail.com>
2022-01-21 17:55:31 +0100

    Change pathing in server for frontend build

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
